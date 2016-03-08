71abaed3c70625d190485cc5ca895b0d247e7a52
adamfollmer <follmeradam@gmail.com>
2016-03-08 14:40:11 -0600

    Creating most member variables and constructors except for AI, which will be decided later

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
 
 public class Player extends Character{
-	ArrayList backpack;
-	Weapon weapon = starterWeapon;
-	Armor armor = starterArmor;
-	Spells[] spells = {frostbolt, fireball, arcaneExplosion};
+	ArrayList<String> backpack; //= new ArrayList<String>();
+	Weapon weapon = new Weapon ("Starter Sword", 1);
+	Armor armor = new Armor ("Starter Armor", 1);
+	Spell arcaneBlast = new Spell ("Arcane Blast", 1);
+	Spell frostbolt = new Spell ("Frostbolt", 2);
+	Spell fireball = new Spell ("Fireball", 3);
+	Spell[] spells = {arcaneBlast, frostbolt, fireball};
 	int LimitBreakStatus = 0;
 	
-	public Player () {
-		super();as
+	public Player (String name, int health) {
+		super(name, health);
 	}
 }
