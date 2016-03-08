b18eb352bd150cea1a648fe4748ca3a9148cda3b
adamfollmer <follmeradam@gmail.com>
2016-03-08 14:08:23 -0600

    Fixing not having a source folder

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+
+public class Character {
+	String name;
+	int health;
+	
+	Character (String Name, int Health) {
+		name = Name;
+		health = Health;
+	}
+}
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+
+public class Player extends Character{
+	ArrayList backpack;
+	Weapon weapon = starterWeapon;
+	Armor armor = starterArmor;
+	Spells[] spells = {frostbolt, fireball, arcaneExplosion};
+	int LimitBreakStatus = 0;
+	
+	public Player () {
+		super();as
+	}
+}
